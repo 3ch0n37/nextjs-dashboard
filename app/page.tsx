a50e4325163d46667b8d2981718cd1565e3a32ed
Vladimir Rujević <vladimir.rujevic@live.com>
2024-05-09 21:13:45 +0200

    Reformat with Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -36,14 +36,14 @@ export default function Page() {
             src="/hero-desktop.png"
             width={1000}
             height={760}
-            className='hidden md:block'
+            className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
-          <Image 
+          <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className='block md:hidden'
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
